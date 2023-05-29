54ce4cb6170a9cceae652ba77cddf59e78399533
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-05-29 15:22:33 +0200

    Add display function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 if (localStorage.getItem('collection')) {
   
     collection = JSON.parse(localStorage.getItem('collection'));
+    displayBooks(collection);
   } else {
     collection = [];
   }
@@ -19,4 +20,35 @@ if (localStorage.getItem('collection')) {
     localStorage.setItem('collection', JSON.stringify(collection));
  
   });
-  console.log(collection) 
+  console.log(collection)
+
+function displayBooks(collection){
+  // const bookElement = document.getElementById("container");
+  // bookElement.innerHTML = collection.map((book, index) =>
+  // `<div>
+  //     <p>${book.title}</p>
+  //     <p>${book.author}</p>
+  //     <button onclick="deleteBooks(${index})" id="remove" >remove</button>
+  //   </div>
+  //   <hr><br>`
+  // )
+  // .join('');
+  collection.forEach((book, index) => {
+    const bookElement = document.createElement('div');
+    bookElement.innerHTML = `<p>Title: ${book.title}</p><p>Author: ${book.author}</p>`;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => {
+      deleteBooks(index);
+      displayBooks(); // Refresh the display after deletion
+    });
+
+    bookElement.appendChild(deleteButton);
+    document.getElementById('container').appendChild(bookElement);
+  });
+}
+
+function deleteBooks(index){
+  collection.splice(index, 1);
+}
