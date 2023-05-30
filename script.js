892897693d8bad919da31ee56ae8095f55e214c8
Batool Fatima <batoolfatima2135@gmail.com>
2023-05-30 01:55:58 -0700

    Add bookList class to sript.js

--- script.js
+++ script.js
@@ -1,52 +1,61 @@
-let collection = [
-  {
-    title: 'Lorem Ipsum',
-    author: 'Testero Testyy',
-  },
-  {
-    title: 'Second Book',
-    author: 'Testero Testyy',
-  },
-];
 
-function addBooks(title, author) {
-  collection.push({
-    title,
-    author,
-  });
+class bookList {
+  constructor()
+  {
+    this.collection = [];
+
+  }
+  addBooks(title, author) {
+    collection.push({
+      title,
+      author,
+    });
+  }
+  displayBooks(collection) {
+    const bookSection = document.querySelector('.books');
+    bookSection.innerHTML = collection.map((book, index) => `
+      <div>
+          <p>${book.title}</p>
+          <p>${book.author}</p>
+          <button type="button" onclick="deleteBooks(${index})" id="remove" >remove</button>
+          <hr>
+      </div>
+      `).join('');
+  }
+  /* eslint-disable no-unused-vars */
+    deleteBooks(index) {
+    collection.splice(index, 1);
+    
+  }
+  
+
 }
 
-function displayBooks(collection) {
-  const bookSection = document.querySelector('.books');
-  bookSection.innerHTML = collection.map((book, index) => `
-    <div>
-        <p>${book.title}</p>
-        <p>${book.author}</p>
-        <button type="button" onclick="deleteBooks(${index})" id="remove" >remove</button>
-        <hr>
-    </div>
-    `).join('');
-}
-/* eslint-disable no-unused-vars */
-function deleteBooks(index) {
-  collection.splice(index, 1);
-  displayBooks(collection);
+
+function deleteBooks(index)
+{
+  const book = new bookList();
+  book.deleteBooks(index);
+  book.displayBooks(collection);
   localStorage.setItem('collection', JSON.stringify(collection));
 }
 
-function logSubmit(event) {
+
+ function  logSubmit(event) {
   const bookTitle = document.querySelector('#book-title').value;
   const bookAuthor = document.querySelector('#book-author').value;
-  addBooks(bookTitle, bookAuthor);
-  displayBooks(collection);
+  const newBook = new bookList();
+  newBook.addBooks(bookTitle, bookAuthor);
+  newBook.displayBooks(collection);
   localStorage.setItem('collection', JSON.stringify(collection));
   event.preventDefault();
 }
 
 document.querySelector('#submit-button').addEventListener('submit', (e) => logSubmit(e));
+const newBook = new bookList();
 if (localStorage.getItem('collection') !== null) {
   collection = JSON.parse(localStorage.getItem('collection'));
-  displayBooks(collection);
+  newBook.displayBooks(collection);
 } else {
-  displayBooks(collection);
+  newBook.displayBooks(collection);
 }
