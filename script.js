47d8ed939ffac059dc8f46286e87f712cf5df1e9
Batool Fatima <batoolfatima2135@gmail.com>
2023-05-30 08:21:09 -0700

    Solve linter errors

--- script.js
+++ script.js
@@ -1,52 +1,53 @@
-class bookList {
+class BookList {
   constructor() {
-    this.collection = [];
+    if (localStorage.getItem('collection') !== null) {
+      this.collection = JSON.parse(localStorage.getItem('collection'));
+    } else {
+      this.collection = [];
+    }
   }
 
   addBooks(title, author) {
-    collection.push({
+    this.collection.push({
       title,
       author,
     });
   }
 
-  displayBooks(collection) {
+  displayBooks() {
     const bookSection = document.querySelector('.books');
-    bookSection.innerHTML = collection.map((book, index) => `
+    bookSection.innerHTML = this.collection.map((book, index) => `
       <tr>
           <td>"${book.title}" by ${book.author}</td>
           <td><button type="button" class="btn btn-primary" onclick="deleteBooks(${index})" id="remove" >remove</button></td>
           
       </tr>
       `).join('');
+    localStorage.setItem('collection', JSON.stringify(this.collection));
   }
 
   /* eslint-disable no-unused-vars */
   deleteBooks(index) {
-    collection.splice(index, 1);
+    this.collection.splice(index, 1);
   }
 }
 
 function deleteBooks(index) {
-  const book = new bookList();
+  const book = new BookList();
   book.deleteBooks(index);
-  book.displayBooks(collection);
-  localStorage.setItem('collection', JSON.stringify(collection));
+  book.displayBooks();
 }
 
 function logSubmit(event) {
   const bookTitle = document.querySelector('#book-title').value;
   const bookAuthor = document.querySelector('#book-author').value;
-  const newBook = new bookList();
+  const newBook = new BookList();
   newBook.addBooks(bookTitle, bookAuthor);
-  newBook.displayBooks(collection);
-  localStorage.setItem('collection', JSON.stringify(collection));
+  newBook.displayBooks();
+
   event.preventDefault();
 }
 
 document.querySelector('#submit-button').addEventListener('submit', (e) => logSubmit(e));
-const newBook = new bookList();
-if (localStorage.getItem('collection') !== null) {
-  collection = JSON.parse(localStorage.getItem('collection'));
-  newBook.displayBooks(collection);
-}
+const newBook = new BookList();
+newBook.displayBooks();
