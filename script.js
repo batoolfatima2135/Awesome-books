1686eb38eaa02be3ed0680dd8d9dd95332bec1c6
Batool Fatima <batoolfatima2135@gmail.com>
2023-05-31 07:02:38 -0700

    Use previously unused function

--- script.js
+++ script.js
@@ -14,30 +14,28 @@ class BookList {
     });
   }
 
+  deleteBooks(index) {
+    this.collection.splice(index, 1);
+  }
+
   displayBooks() {
     const bookSection = document.querySelector('.books');
     bookSection.innerHTML = this.collection.map((book, index) => `
       <tr>
           <td>"${book.title}" by ${book.author}</td>
-          <td><button type="button" class="btn btn-primary" onclick="deleteBooks(${index})" id="remove" >remove</button></td>
+          <td><button type="button" class="btn btn-primary" onclick="deleteBooks(${index})" class="remove" >remove</button></td>
           
       </tr>
       `).join('');
+
     localStorage.setItem('collection', JSON.stringify(this.collection));
   }
-
-  /* eslint-disable no-unused-vars */
-  deleteBooks(index) {
-    this.collection.splice(index, 1);
-  }
 }
-
 function deleteBooks(index) {
   const book = new BookList();
   book.deleteBooks(index);
   book.displayBooks();
 }
-
 function logSubmit(event) {
   const bookTitle = document.querySelector('#book-title').value;
   const bookAuthor = document.querySelector('#book-author').value;
@@ -49,7 +47,7 @@ function logSubmit(event) {
   event.preventDefault();
 }
 
-document.querySelector('#submit-button').addEventListener('submit', (e) => logSubmit(e));
+document.querySelector('#submit-button').addEventListener('submit', () => logSubmit());
 const newBook = new BookList();
 newBook.displayBooks();
 
@@ -67,7 +65,7 @@ const contactContainer = document.getElementById('contact');
 const list = document.getElementById('book-list');
 const bookForm = document.getElementById('book-form');
 const contactInfo = document.getElementById('contact-info');
-myLinksContainer.addEventListener('click', (e) => {
+myLinksContainer.addEventListener('click', () => {
   bookForm.style.display = 'none';
   contactInfo.style.display = 'none';
   list.style.display = 'block';
@@ -75,7 +73,7 @@ myLinksContainer.addEventListener('click', (e) => {
   contactContainer.classList.remove('active');
   myLinksContainer.classList.add('active');
 });
-addContainer.addEventListener('click', (e) => {
+addContainer.addEventListener('click', () => {
   list.style.display = 'none';
   contactInfo.style.display = 'none';
   bookForm.style.display = 'block';
@@ -83,7 +81,7 @@ addContainer.addEventListener('click', (e) => {
   contactContainer.classList.remove('active');
   addContainer.classList.add('active');
 });
-contactContainer.addEventListener('click', (e) => {
+contactContainer.addEventListener('click', () => {
   bookForm.style.display = 'none';
   list.style.display = 'none';
   contactInfo.style.display = 'block';
@@ -91,3 +89,4 @@ contactContainer.addEventListener('click', (e) => {
   myLinksContainer.classList.remove('active');
   contactContainer.classList.add('active');
 });
+deleteBooks();
